d3f94556092dcc1d8996b7478969c23cf1da0a9f
Guilherme Guimarães <guima_dev@hotmail.com>
2019-03-28 07:18:58 -0300

    Adicionado mais comentários e adição de arestas à lista de adijacência

--- estudos_grafos/Entities/Grafo.cs
+++ estudos_grafos/Entities/Grafo.cs
@@ -9,17 +9,17 @@ namespace estudos_grafos.Entities
         //Atributos da classe Grafo
         public int N { get; set; } // Número de vértices
         public int M { get; set; } // Número de arestas
-        public List<Vertice>[] ListaAdjacencia{ get; set; } // Lista de adjacência sem rótulos
-        public int[,] MatrizAdjacencia;
-        public Vertice [] Rotulos { get; set; } //Vetor de Rótulos dos vértices
+        public List<int>[] ListaAdjacencia{ get; set; } // Lista de adjacência sem rótulos
+        public int[,] MatrizAdjacencia;//Matriz de adjacência do grafo
+        public Vertice [] Vertices { get; set; } //Vetor de Rótulos dos vértices
         
-        //CONTRUTOR DO GRAFO
+        //CONSTRUTOR DA CLASSE
         public Grafo(int n)
         {
             N = n;//Atribui o parâmetro de construção á propriedade "N" 
             MatrizAdjacencia = new int[N, N]; //Inicializa a matriz de adjacência 
-            ListaAdjacencia = new List<Vertice>[N]; // Inicializa a lista de adjacência(necessita implementar)
-            Rotulos = new Vertice[N]; //Inicializa o vetor de rótulos dos vértices
+            ListaAdjacencia = new List<int>[N]; // Inicializa a lista de adjacência(necessita implementar)
+            Vertices = new Vertice[N]; //Inicializa o vetor de rótulos dos vértices
             for(int i = 0; i < N; i++)
             {
                 for(int j = 0; j < N; j++)
@@ -34,17 +34,17 @@ namespace estudos_grafos.Entities
             if (!v.isNull()) {
                 for (int i = 0; i < N; i++)
                 {
-                    if (Rotulos[i] == null)
+                    if (Vertices[i] == null)
                     {
-                        Rotulos[i] = v;
+                        Vertices[i] = v;
                         break;
                     }
                 }
             }
         }
 
-        public bool adicionaAresta(string aresta)
-        {
+        public bool adicionaAresta(string aresta) //Adiciona aresta verificando se foi enviado uma sentença correta como padrão descrito(Vertice-Vertice)
+        {                                         //verificando se é um par de rótulos separados por hífen(-)
             string[] aux = aresta.Split('-');
             int posVertice1;
             int posVertice2;
@@ -52,12 +52,14 @@ namespace estudos_grafos.Entities
             {
                 return false;
             }
-            posVertice1 = existeVertice(aux[0]);
-            posVertice2 = existeVertice(aux[1]);
+            posVertice1 = existeVertice(aux[0]);//Verifica se o primeiro vértice existe procurando seu rótulo no vetor de vértices.
+            posVertice2 = existeVertice(aux[1]);//Verifica se o segundo vértice existe procurando seu rótulo no vetor de vértices.
             if(posVertice1 != -1 && posVertice2 != -1)
             {
                 MatrizAdjacencia[posVertice1, posVertice2] = 1;
                 MatrizAdjacencia[posVertice2, posVertice1] = 1;
+                ListaAdjacencia[posVertice1].Add(posVertice2);
+                ListaAdjacencia[posVertice2].Add(posVertice1);
             }
             else
             {
@@ -69,11 +71,11 @@ namespace estudos_grafos.Entities
             
         }
 
-        public void imprimeMatriz()
+        public void imprimeMatriz()//Imprime a matriz de adjacência com formatação bem furreca
         {
             for(int i = 0; i < N; i++)
             {
-                Console.Write(Rotulos[i].Rotulo + "");
+                Console.Write(i + " ");
                 for(int j = 0; j < N; j++)
                 {
                     Console.Write(MatrizAdjacencia[i, j] + " ");
@@ -83,11 +85,19 @@ namespace estudos_grafos.Entities
             }
         }
 
-        private int existeVertice(string rotulo)
+        public void imprimeLista()
         {
             for(int i = 0; i < N; i++)
             {
-                if(Rotulos[i].Rotulo == rotulo && !Rotulos[i].isNull())
+
+            }
+        }
+
+        private int existeVertice(string rotulo)//Procura o rótulo do vértice parametrizado, se existir retorna o índice, else, retorna -1
+        {
+            for(int i = 0; i < N; i++)
+            {
+                if(Vertices[i].Rotulo == rotulo && !Vertices[i].isNull())
                 {
                     return i; 
                 }
